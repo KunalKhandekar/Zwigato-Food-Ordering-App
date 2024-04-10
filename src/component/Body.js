50018035b9b4cbcccdb12ca11bc57ba35990607c
KunalKhandekar <kunalkhandekar010@gmail.com>
2024-04-10 18:24:45 +0530

    Search Bar

--- src/component/Body.js
+++ src/component/Body.js
@@ -6,6 +6,8 @@ import ShimmerUI from "./ShimmerUI";
 const Body = () => {
 
     let [listOfResturants, setlistOfResturants] = useState([]);
+    let [filteredResturantsList, setfilteredResturantsList] = useState([]);
+    let [searchText, setsearchText] = useState('');
 
 
     useEffect(() => {
@@ -13,11 +15,12 @@ const Body = () => {
     }, []);
 
     const fetchData = async () => {
-        const data = await fetch("https://swiggy.com/dapi/restaurants/list/v5?lat=19.07480&lng=72.88560");
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.07480&lng=72.88560");
 
         const jsonData = await data.json();
 
         setlistOfResturants(jsonData?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setfilteredResturantsList(jsonData?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
     };
 
 
@@ -30,23 +33,66 @@ const Body = () => {
         (
             <div className='main-container'>
 
-                <button className="filter-btn" onClick={() => {
-                    const filteredList = listOfResturants.filter((resturant) => (
-                        resturant.info.avgRating > 4
-                    ));
-                    setlistOfResturants(filteredList);
-                    console.log(filteredList)
-                }}>
-                    Top Rated Returants
-                </button >
+                <div className="filter">
+
+                    <button className="filter-btn" onClick={() => {
+                        const filteredList = listOfResturants.filter((resturant) => (
+                            resturant.info.avgRating > 4
+                        ));
+                        setfilteredResturantsList(filteredList);
+                    }}>
+                        Top Rated Returants
+                    </button >
+
+                    <div className="search">
+                        <input
+                            type="text"
+                            placeholder="Search"
+                            className="search-input"
+                            value={searchText}
+                            onChange={(e) => {
+                                setsearchText(e.target.value)
+                            }}
+                        />
+
+                        <button
+                            className="search-btn"
+                            onClick={() => {
+
+                                // For Filtering from names
+                                const filteredRestaurants1 = listOfResturants.filter((res) => (
+                                    res.info?.name?.toLowerCase().includes(searchText.toLowerCase())
+                                ));
+
+
+                                // For filtering from Cuisines
+                                const filteredRestaurants2 = listOfResturants.filter((res) => {
+                                    const hasMatchingCuisine = res.info.cuisines.some((cuisine) =>
+                                        cuisine.toLowerCase().includes(searchText.toLowerCase())
+                                    );
+                                
+                                    return hasMatchingCuisine;
+                                });
+
+                                // Merging both and removing the duplicate. 
+                                const filteredRestaurants = Array.from(new Set(filteredRestaurants1.concat(filteredRestaurants2)));
+                                
+                                setfilteredResturantsList(filteredRestaurants);
+                                setsearchText('');
+                            }}
+
+                        >Search</button>
+                    </div>
+
+                </div>
 
 
                 <div className="restruant-container">
-                    {listOfResturants.map(resturant => (
+                    {filteredResturantsList.map(resturant => (
                         <RestrauntCard ResData={resturant} />
                     ))}
                 </div>
-            </div>
+            </div >
         )
 };
 
