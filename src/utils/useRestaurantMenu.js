db6f91d77f1e4d26b72ede3225b347c3cfeeafda
KunalKhandekar <kunalkhandekar010@gmail.com>
2024-04-19 14:34:23 +0530

    API

--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -2,30 +2,42 @@ import { useState, useEffect } from "react";
 import { isMobile } from "react-device-detect";
 
 const useRestaurantMenu = (resID) => {
-    const [restaurantDetails, setrestaurantDetails] = useState([]);
-    const [menuDetails, setmenuDetails] = useState([]);
+    const [restaurantDetails, setRestaurantDetails] = useState([]);
+    const [menuDetails, setMenuDetails] = useState([]);
 
     useEffect(() => {
         fetchMenu();
     }, []);
 
     const fetchMenu = async () => {
-        const locationData = await fetch('https://ipapi.co/json');
-        const locationJSON = await locationData.json();
-        const url = isMobile ?
-        `https://thingproxy-760k.onrender.com/fetch/https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=${locationJSON.latitude}&lng=${locationJSON.longitude}&restaurantId=${resID}&isMenuUx4=true&submitAction=ENTER`
-        :
-        `https://thingproxy-760k.onrender.com/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=26.8947446&lng=75.8301169&restaurantId=${resID}&catalog_qa=undefined&submitAction=ENTER`
-        ;
-        const response = await fetch(url);
-        const json1 = await response.json();
-        const apiData = isMobile ? json1?.data?.cards[5]  : json1?.data?.cards[4]
-        setrestaurantDetails(json1?.data?.cards[2]?.card?.card);
-        setmenuDetails(apiData?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter((menuList) => (
-            menuList?.card?.card?.['@type'] == 'type.googleapis.com/swiggy.presentation.food.v2.ItemCategory'
-        )));
-
+        try {
+            const locationData = await fetch('https://ipapi.co/json');
+            const locationJSON = await locationData.json();
+            console.log("Location JSON:", locationJSON);
+    
+            const url = isMobile ?
+                `https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=${locationJSON.latitude}&lng=${locationJSON.longitude}&restaurantId=${resID}&isMenuUx4=true&submitAction=ENTER`
+                :
+                `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=${locationJSON.latitude}&lng=${locationJSON.longitude}&restaurantId=${resID}&catalog_qa=undefined&submitAction=ENTER`;
+    
+            const response = await fetch(`https://proxy.cors.sh/${url}`, {
+                headers: {
+                'x-cors-api-key': 'temp_08a8b551696538e5f949f708a4483487'
+                }
+              });
+            const jsonData = await response.json();
+            console.log("Fetched JSON Data:", jsonData); // Log the fetched JSON data
+    
+            const apiData = isMobile ? jsonData?.data?.cards[5] : jsonData?.data?.cards[4];
+            setRestaurantDetails(jsonData?.data?.cards[2]?.card?.card);
+            setMenuDetails(apiData?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter((menuList) => (
+                menuList?.card?.card?.['@type'] === 'type.googleapis.com/swiggy.presentation.food.v2.ItemCategory'
+            )));
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
     };
+    
 
     return {
         restaurantDetails: restaurantDetails,
